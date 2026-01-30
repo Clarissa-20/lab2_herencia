1af03026cc189dd8b92d708ec05a9e16bdc5a910
Clarissa Ramos <angie.ramos20137@gmail.com>
2026-01-30 09:32:33 -0600

    clase empleado temporal

--- lab2Herencia/src/lab2herencia/EmpleadoTemporal.java
+++ lab2Herencia/src/lab2herencia/EmpleadoTemporal.java
@@ -3,11 +3,47 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package lab2herencia;
+import java.util.Calendar;
 
 /**
  *
  * @author HP
  */
-public class EmpleadoTemporal {
+public class EmpleadoTemporal extends Empleado{
+     private Calendar fechaFinContrato;
+
+    public EmpleadoTemporal(int codigo, String nombre, double salarioBase, Calendar fechaFinContrato) {
+        super(codigo, nombre, salarioBase);
+        this.fechaFinContrato = fechaFinContrato;
+    }
+
+    @Override
+    public double calculoPago() {
+        Calendar hoy = Calendar.getInstance();
+        if (hoy.after(fechaFinContrato)) {   // más claro que compareTo
+            return 0.0;
+        }
+        return super.calculoPago();
+    }
+
+    public void actualizarFechaFinContrato(Calendar nuevaFecha) {
+        this.fechaFinContrato = nuevaFecha;
+    }
+
+    public Calendar getFechaFinContrato() {
+        return fechaFinContrato;
+    }
+
+    @Override
+    public String toString() {
+        String infoBase = super.toString();
+
+        String fechaFinFormateada = String.format("%02d/%02d/%d",
+                fechaFinContrato.get(Calendar.DAY_OF_MONTH),
+                fechaFinContrato.get(Calendar.MONTH) + 1,
+                fechaFinContrato.get(Calendar.YEAR));
+
+        return infoBase + " | Fin contrato: " + fechaFinFormateada;
+    }
     
 }
