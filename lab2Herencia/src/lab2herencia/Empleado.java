1a9b942ca1242418fab31312a0108bf5358f4727
Joshua Stan <lego24451@gmail.com>
2026-01-30 09:29:52 -0600

    creacion de clase intento 2 Empelado

--- lab2Herencia/src/lab2herencia/Empleado.java
+++ lab2Herencia/src/lab2herencia/Empleado.java
@@ -4,10 +4,52 @@
  */
 package lab2herencia;
 
+import java.util.Calendar;
+
 /**
  *
  * @author spodi
  */
 public class Empleado {
+    protected int codigo;
+    protected String nombre;
+    protected Calendar fechaCont;
+    protected double salarioBase;
+    protected int horasTotal;
+
+    public Empleado(int codigo, String nombre, double salarioBase) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.fechaCont = Calendar.getInstance(); 
+        this.salarioBase = salarioBase;
+        this.horasTotal = 0; 
+    }
+
+    public int getCodigo() { return codigo; }
+    public String getNombre() { return nombre; }
+    public Calendar getFechaCont() { return fechaCont; }
+    public double getSalarioBase() { return salarioBase; }
+    public int getHorasTotal() { return horasTotal; }
+
+    public void horasTrabajadas(int horas) {
+        if (horas > 0) {
+            horasTotal += horas;
+        }
+    }
+
     
+    public double calculoPago() {
+        return (horasTotal / 160.0) * salarioBase;
+    }
+
+    public String mostrarInformacion() {
+        return "Código: " + codigo +
+               " | Nombre: " + nombre +
+               " | Fecha de contratación: " + fechaCont.getTime();
+    }
+
+    @Override
+    public String toString() {
+        return mostrarInformacion();
+    }
 }
